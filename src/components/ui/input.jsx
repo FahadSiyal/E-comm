809c0778f92a996fe3829010469796bd7a21ac10
Fahad Siyal <fahadsiyal748@gmail.com>
2025-04-30 17:25:27 +0500

    Update input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,7 +1,12 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
+import * as React from "react"
 
-function Input({ className, type, ...props }) {
+import { cn } from "@/lib/utils"
+
+function Input({
+  className,
+  type,
+  ...props
+}) {
   return (
     <input
       type={type}
@@ -12,9 +17,8 @@ function Input({ className, type, ...props }) {
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
-      {...props}
-    />
+      {...props} />
   );
 }
 
-export { Input };
+export { Input }
