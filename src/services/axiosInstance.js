f0b2cd582449bd86609a0f5626530e5b76820d91
AarijFarhan <aarijhere10@gmail.com>
2025-05-02 07:53:00 -0700

    .

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://clothbackend-umber.vercel.app/api',
+    baseURL: 'https://clothingback.vercel.app/api/',
     // baseURL: 'http://localhost:3000/api',
    
 
