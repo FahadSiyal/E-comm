79106cdfd910734c08f1de9b59ee106757552665
Fahad Siyal <fahadsiyal748@gmail.com>
2025-05-02 18:09:03 +0500

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -357,7 +357,8 @@ const Navbar = () => {
                         }}
                         className="py-5 text-black "
                       >
-                        <span className=" mt-2 border rounded-md hover:bg-gray-400 hover:text-white  px-4 py-2">
+                        <span className=" mt-2 b
+                        order rounded-md hover:bg-gray-400 hover:text-white  px-4 py-2">
                           Signup{" "}
                         </span>
                       </button>
