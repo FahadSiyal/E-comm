6b56899d2005306634636a1c17f2310ee4b3b26a
Fahad Siyal <fahadsiyal748@gmail.com>
2025-05-07 16:23:24 +0500

    Update axiosInstance.js

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
     baseURL: 'https://cloth-backend-2kmewue4o-aarijs-projects-e50dbb08.vercel.app/api',
     // baseURL: 'http://localhost:3000/api',
     // baseURL: 'https://cloth-backend-pwni.vercel.app/api',
-    baseURL: 'https://ecomcloth1.netlify.app/shop',
+    // baseURL: 'https://ecomcloth1.netlify.app/shop',
    
 
     withCredentials: true,
