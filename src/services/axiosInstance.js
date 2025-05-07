aca7522678ebade708bcc7ce2baf31da799a5e16
AarijFarhan <aarijhere10@gmail.com>
2025-05-07 05:40:53 -0700

    a

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://cloth-backend-2kmewue4o-aarijs-projects-e50dbb08.vercel.app/api',
-    // baseURL: 'http://localhost:3000/api',
+    // baseURL: 'https://cloth-backend-2kmewue4o-aarijs-projects-e50dbb08.vercel.app/api',
+    baseURL: 'http://localhost:3000/api',
    
 
     withCredentials: true,
