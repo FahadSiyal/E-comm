6701b2144b355b758174b2e26d410ca64d8f7756
Fahad Siyal <fahadsiyal748@gmail.com>
2025-06-19 19:23:00 +0500

    ..

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
     // baseURL: 'https://clothbackenddepolyment.vercel.app/api',
-    // baseURL: 'http://localhost:3000/api',
-    baseURL: 'https://cloth-backend-pwni.vercel.app/api',
+    baseURL: 'http://localhost:3000/api',
+    // baseURL: 'https://cloth-backend-pwni.vercel.app/api',
     // baseURL: 'https://ecomcloth1.netlify.app/shop',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        // 'Accept': 'application/json',
     },
 })
 
