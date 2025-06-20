ef21aa4cc19906d5f318785539e4ad6128add4df
AarijFarhan <aarijhere10@gmail.com>
2025-06-20 12:48:11 -0700

    ,

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const axiosInstance = axios.create({
     // baseURL: 'https://clothbackenddepolyment.vercel.app/api',
     // baseURL: 'http://localhost:3000/api',
-    baseURL: 'https://cloth-backend-pwni.vercel.app/api',
+    baseURL: 'https://cloth-backend-pwni.vercel.app/ap   i',
     // baseURL: 'https://ecomcloth1.netlify.app/shop',
     withCredentials: true,
     headers: {
